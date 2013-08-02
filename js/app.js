fd3754aaff4b35a23707b61340d85b41a58335ad
Eremin Andrey <eremin_av@masterbank.ru>
2013-08-02 11:53:33 +0400

    Work in models.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.resource('todos', { path: '/' });
+});
+
+App.InfoBlock = DS.Model.extend({
+  title: DS.attr('string'),
+  description: DS.attr('string'),
+  link: DS.attr('string'),
+  use_link: DS.attr('boolean'),
+
+  fullName: function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName')
 });
 
 App.IndexRoute = Ember.Route.extend({
