5e5aaa0d331880535463106be2a29b600e63f5ef
Andrey Eremin <dsoft88@gmail.com>
2015-08-23 12:55:22 +0200

    Removed old files.

--- js/data.js
+++
@@ -1,8 +0,0 @@
-// Defines Info Blocks
-App.InfoBlock.FIXTURES = [
-  { id: 1, title: 'YouTube', description: 'Последние добавленные видеозаписи', link: 'http://www.youtube.com/user/Dsoft88', content_type: 'YouTube' },
-  { id: 2, title: 'Blogger' , description: 'Последние записи в блог', link: 'http://briefchit.blogspot.ru/', content_type: 'Blogger' },
-  { id: 3, title: 'GitHub' , description: 'Проекты', link: 'https://github.com/developer88', content_type: 'GitHub' },
-  { id: 4, title: 'DeviantArt' , description: 'Графика', link: 'http://developer88.deviantart.com/gallery/', content_type: 'DeviantArt' }
-  { id: 5, title: 'Google+' , description: 'Мои последние публичные записи в Google+', link: 'https://plus.google.com/u/0/102960425588359758591/posts', content_type: 'GooglePlus' }
-];
